e7bc066a087d49f0409c4f83ca71341946363fa4
Slime <4sliman4@gmail.com>
2024-12-26 09:17:49 +0200

    Fix editing + sort by successes

--- crates/dashboard-frontend/api/oracles.ts
+++ crates/dashboard-frontend/api/oracles.ts
@@ -1,3 +1,5 @@
+import { EventStreamClient } from "@intear/inevents-websocket-client";
+
 export const ORACLE_CONTRACT_ID = "dev-unaudited-v1.oracle.intear.near";
 
 export interface Oracle {
@@ -24,4 +26,74 @@ export async function fetchOracles(): Promise<Oracle[]> {
         console.error('Error fetching oracles:', error);
         return [];
     }
-} 
+}
+
+interface LogNep297Event<T> {
+    block_height: number;
+    block_timestamp_nanosec: string;
+    transaction_id: string;
+    receipt_id: string;
+    account_id: string;
+    predecessor_id: string;
+    event_standard: string;
+    event_version: string;
+    event_event: string;
+    event_data: T;
+}
+
+interface Producer {
+    account_id: string;
+    requests_succeded: number;
+    requests_timed_out: number;
+    fee: ProducerFee;
+    send_callback: boolean;
+    name: string;
+    description: string;
+    example_input?: string;
+}
+
+type ProducerFee = 
+    | "None"
+    | { Near: { prepaid_amount: string } }
+    | { FungibleToken: { token: string, prepaid_amount: string } };
+
+export async function listenForUpdates(onOracleUpdate: (oracle: Oracle) => void) {
+    const client = EventStreamClient.default();
+    await client.streamEvents<LogNep297Event<Producer>>("log_nep297", async (event) => {
+        if (event.account_id === ORACLE_CONTRACT_ID && event.event_standard === "intear-oracle") {
+            if (event.event_event === "producer_created" || event.event_event === "producer_updated") {
+                let producer: Producer = event.event_data;
+                let oracle: Oracle = {
+                    id: producer.account_id,
+                    name: producer.name,
+                    description: producer.description,
+                    successes: producer.requests_succeded,
+                    failures: producer.requests_timed_out,
+                    example_input: producer.example_input,
+                    fee: (() => {
+                        if (producer.fee === "None") {
+                            return {
+                                amount: "0",
+                                token: "near"
+                            };
+                        }
+                        if ('Near' in producer.fee) {
+                            return {
+                                amount: producer.fee.Near.prepaid_amount,
+                                token: "near"
+                            };
+                        }
+                        if ('FungibleToken' in producer.fee) {
+                            return {
+                                amount: producer.fee.FungibleToken.prepaid_amount,
+                                token: producer.fee.FungibleToken.token
+                            };
+                        }
+                        throw new Error('Unknown fee type');
+                    })()
+                }
+                onOracleUpdate(oracle);
+            }
+        }
+    });
+}
