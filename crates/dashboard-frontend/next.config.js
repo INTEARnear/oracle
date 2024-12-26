d26ac802e404c537061539b43830607b1223a340
Slime <4sliman4@gmail.com>
2024-12-26 11:59:12 +0200

    Deploy dashboard frontend on github pages

--- crates/dashboard-frontend/next.config.js
+++ crates/dashboard-frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig 
